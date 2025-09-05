0e25de75e0efbefeba42d0ae0e874b6ffc1a685c
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-09-05 11:14:16 +0200

    5

--- guest_list.py
+++ guest_list.py
@@ -25,3 +25,33 @@ guests.append("bruce lee")
 
 message = f"Dear {guests} I would like cordially invite you to dinner"
 print(message)
+
+print("Sorry but now I can only invite two people for dinner")
+
+rejected_guest = guests.pop()
+new_message = f"sorry {rejected_guest}, but you are not welcome anymore"
+print(new_message)
+
+rejected_guest = guests.pop()
+new_message = f"sorry {rejected_guest}, but you are not welcome anymore"
+print(new_message)
+
+rejected_guest = guests.pop()
+new_message = f"sorry {rejected_guest}, but you are not welcome anymore"
+print(new_message)
+
+rejected_guest = guests.pop()
+new_message = f"sorry {rejected_guest}, but you are not welcome anymore"
+print(new_message)
+
+final_message = f"Don't worry {guests} you are still invited"
+print(final_message)
+
+del guests[0]
+del guests[0]
+guests = guests
+guests.append("...")
+
+final_message = f"Don't worry {guests} you are still invited"
+print(final_message)
+

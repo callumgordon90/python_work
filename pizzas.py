571b0558641f7e3d3626af1a66b85ac49c9cf954
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-09-05 13:00:39 +0200

    commit 5

---
+++ pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ["pepperoni", "hawaian", "ham and cheese", "bbq"]
+
+for pizza in pizzas:
+    print(pizza)
+    print(f"I love {pizza} \n")
+print("spank my wasp if that isn't all we've got time for")

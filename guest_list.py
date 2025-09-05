111b2902531f45d8b33df1517de48b5d1e83fcb8
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-09-05 10:47:49 +0200

    commit 4

---
+++ guest_list.py
@@ -0,0 +1,27 @@
+guests = ['julius ceaser' , 'albert einstein' , 'plato' , 'jesus christ']
+
+absentee= guests.pop(1)
+
+absentee= "elvis presley"
+
+
+message = f"Dear {guests[0].title()} I would like cordially invite you to dinner"
+print(message)
+
+message = f"Dear {guests[1].title()} I would like cordially invite you to dinner"
+print(message)
+
+message = f"Dear {guests[2].title()} I would like cordially invite you to dinner"
+print(message)
+
+
+
+news = "i found a bigger motherfucking table!"
+print(news.upper())
+
+guests.insert(0, "tupac shakur")
+guests.insert(2, "marilyn monroe")
+guests.append("bruce lee")
+
+message = f"Dear {guests} I would like cordially invite you to dinner"
+print(message)

9fdf1cb9f0d942cb085aebf88c70e14a18e7a3ec
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-09-10 12:47:45 +0200

    tuples practice

---
+++ buffet.py
@@ -0,0 +1,9 @@
+basic_foods = ('fish', 'meat', 'bread', 'rice', 'vegetables')
+
+# basic_foods[1] = "seafood"
+
+basic_foods = ('fish', 'seafood', 'bread', 'pasta', 'vegetables')
+
+print("The restaurant offers:")
+for food in basic_foods:
+    print(food)

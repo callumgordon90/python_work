380cbf61e516361d11f836b4eed7a54bc9bbd90c
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-09-10 12:42:24 +0200

    lists, loops review

--- pizzas.py
+++ pizzas.py
@@ -1,6 +1,15 @@
 pizzas = ["pepperoni", "hawaian", "ham and cheese", "bbq"]
 
+friend_pizzas = pizzas[:]
+
+pizzas.append(2)
+
+friend_pizzas.append('napoli')
+
+print('my favourite pizzas are:')
 for pizza in pizzas:
     print(pizza)
-    print(f"I love {pizza} \n")
-print("spank my wasp if that isn't all we've got time for")
+
+print('\nmy friends favourite pizzas are:')
+for pizza in friend_pizzas:
+    print(pizza)

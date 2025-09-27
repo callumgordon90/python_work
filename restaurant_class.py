c8252f3a006fbe2b0228af4c7337a3b56c18a198
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-09-27 18:03:57 +0200

    more restaurant practice

--- restaurant_class.py
+++ restaurant_class.py
@@ -3,6 +3,7 @@ class Restaurant:
         """Initialise the attributes of the restaurant"""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 22
 
     def describe_restaurant(self):
         print(f"The name of the restaurant is: {self.restaurant_name}")
@@ -10,3 +11,22 @@ class Restaurant:
         
     def open_restaurant(self):
         print(f"{self.restaurant_name} is open for business, and spank my wasp if it won\'t be serving {self.cuisine_type} all day!")
+
+    def num_served(self):
+        print(f"This restaurant has served {self.number_served} customers")
+    
+    def set_num_served(self):
+        """This function allows the user to input a number to define the no. of customers"""
+        while True:
+            self.number_served = input("\nHow many customer\'s have we had today pat? \npress 'q' to quit")
+            if self.number_served.lower() == 'q':
+                print("Exiting input.")
+                break
+            print(f"\nwell spank my wasp! we\'ve had {self.number_served} customers!!!")
+    
+    def increment_num_served(self, inc_served):
+        self.number_served += int(inc_served)
+        print(f"Current number of customer\'s in {self.restaurant_name} is {self.number_served}")
+          
+
+            
--- restaurant_instance.py
+++ restaurant_instance.py
@@ -1,11 +1,16 @@
 import restaurant_class as rs
 
 my_restaurant = rs.Restaurant("Cally's Pasty Shop", "Cornish Pasty")
+
 sumny_restuarant = rs.Restaurant("Restaurante de Sumny", "Arroz con judias negras, avocate y platano" )
 pat_thistle_restaurante = rs.Restaurant("Pat's Sausage Emporium" , "Cumberland Sausage")
 
 my_restaurant.describe_restaurant()
-# my_restaurant.open_restaurant()
+my_restaurant.open_restaurant()
+my_restaurant.num_served()
+# my_restaurant.set_num_served()
 
-sumny_restuarant.describe_restaurant()
-pat_thistle_restaurante.describe_restaurant()
+my_restaurant.increment_num_served("22")
+
+# sumny_restuarant.describe_restaurant()
+# pat_thistle_restaurante.describe_restaurant()

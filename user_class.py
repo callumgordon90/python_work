302dbef100ea5c36070ac2b5d7f2f83b94a3bdd2
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-09-27 17:33:38 +0200

    user instance practice

--- user_class.py
+++ user_class.py
@@ -1,13 +1,13 @@
 class User:
     """A user class"""
-    def __init__(self, username, password, age, country):
+    def __init__(self, name, username, age, country):
+        self.name = name
         self.username = username
-        self.password = password
         self.age = age
         self.country = country
     
     def describe_user(self):
-        print(f"Here are the user\'s details: {self.username, self.password, self.age, self.country}")
+        print(f"Here are the user\'s details: \nname: {self.name} \n username:{self.username} \nage: {self.age} \ncountry {self.country}")
     
     def greet_user(self):
         print(f"Hello {self.username}")
--- user_instance.py
+++ user_instance.py
@@ -1,6 +1,10 @@
 import user_class as u
 
-my_user = u.User("cally", "123", "35", "UK")
+my_user = u.User("callum", "cally", "35", "UK")
+sumny_user = u.User('sumny', 'soom sooms', '35', 'Cuba')
 
 my_user.describe_user()
 my_user.greet_user()
+
+sumny_user.describe_user()
+sumny_user.greet_user()

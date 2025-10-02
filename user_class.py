f36097d9bb546a14f60db7fe8eab42047b0b9c01
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-10-02 14:08:47 +0200

    priviliges class

--- user_class.py
+++ user_class.py
@@ -20,3 +20,35 @@ class User:
     def reset_login_attempts(self):
         self.login_attempt = 0
         print(f"{self.username} has attempted to log in {self.login_attempt} times")
+
+class Privileges:
+    """Defining privileges outside the Admin class, then using an instance of 'privileges' as admin attribute"""
+    def __init__(self, privileges=["Hello you silly sausages", "spank my wasp", "right bleedin stinger"]):
+        self.privileges = privileges
+        
+    def show_privileges(self):
+        print(f"Here are the privileges: {self.privileges} \n and here they are listed individually:")
+        for privilege in self.privileges:
+            print(f"- {privilege}")
+
+
+class Admin(User):
+    """Admin is a special type of user. It inherits from the user class and adds priviledges attribute"""
+    def __init__(self, name, username, age, country ):
+        """Now define the inherited attributes with 'super' and define the new attribute"""
+        super().__init__( name, username, age, country )
+        self.privileges = Privileges()
+        
+        
+   
+
+
+
+cally_admin = Admin(
+    "callum",
+    "cally90",
+    "35",
+    "UK",
+)
+
+print(cally_admin.privileges.show_privileges())

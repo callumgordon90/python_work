ef01006fbbfe65e1cc507e8ff4db86591dde10db
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-10-02 13:41:32 +0200

    created admin instance

---
+++ admin_instance.py
@@ -0,0 +1,11 @@
+import admin_class as ac
+
+cally_admin = ac.Admin(
+    "callum",
+    "cally90",
+    "35",
+    "UK",
+    ["Hello you silly sausages", "spank my wasp", "right bleedin stinger"]
+)
+
+cally_admin.show_privileges()
